d3cec2eacdadb055d463dc8cd30ab856bfa65187
Ben Kucera <14625260+Bkucera@users.noreply.github.com>
2019-06-27 15:29:54 -0400

    fix: package.json sorting order
    feat: `json/ignore-files` config (default: ["package-lock.json"])
    
    BREAKING CHANGE:
    
    - rename settings value `json/json-with-comments-filenames` to
    `json/json-with-comments-files`
    
    - change default sorting order of package.json

--- test/main.spec.js
+++ test/main.spec.js
@@ -1,19 +1,18 @@
-const path = require('path')
+const _ = require('lodash')
 const CLIEngine = require('eslint').CLIEngine
-// const semver = require('semver')
-// const eslintVersion = require('eslint/package.json').version
-const { formatJSON, parseJSON, sortPkgJSON } = require('../lib/utils')
 const fs = require('fs-extra')
-const plugin = require('..')
-
-// function ifVersion (versionSpec, fn, ...args) {
-//   const execFn = semver.satisfies(eslintVersion, versionSpec) ? fn : fn.skip
-
-//   execFn(...args)
-// }
+const path = require('path')
+const { formatJSON, parseJSON, sortPkgJSON } = require('../lib/utils')
+let plugin = require('..')
 
 const getFormatted = async (filename) => {
-  return `${formatJSON(parseJSON((await fs.readFile(path.join(__dirname, filename))).toString()))}\n`
+  return formatJSON(
+    parseJSON((await fs.readFile(path.join(__dirname, filename))).toString())
+  )
+}
+
+const getFormattedAndSorted = async (filename) => {
+  return formatJSON(sortPkgJSON(parseJSON(await getFormatted(filename))))
 }
 
 function execute (file, baseConfig) {
@@ -42,229 +41,301 @@ function execute (file, baseConfig) {
   cli.addPlugin('json-format', plugin)
   const results = cli.executeOnFiles([path.join(__dirname, file)]).results[0]
 
-  return baseConfig.fix ? results : results && results.messages
+  return results
 }
 
-it('lint bad json', async () => {
-  const filename = './fixtures/demo.json'
-  const result = execute(filename, {
-    fix: true,
+describe('main spec', () => {
+
+  it('lint bad json', async () => {
+    const filename = './fixtures/demo.json'
+    const result = execute(filename, {
+      fix: true,
+    })
+
+    expect(result.output).toBe(await getFormatted(filename))
+    expect(result.messages).toHaveLength(0)
   })
 
-  expect(result.output).toBe(await getFormatted(filename))
-})
+  it('lint bad json no fix', async () => {
+    const filename = './fixtures/demo.json'
+    const result = execute(filename, {})
 
-it('lint bad json no fix', async () => {
-  const filename = './fixtures/demo.json'
-  const result = execute(filename, {
+    expect(result.messages[0]).toHaveProperty('severity', 2)
+    expect(result.messages[0]).toHaveProperty(
+      'message',
+      'Format Error: expected "\\n" '
+    )
   })
 
-  expect(result[0]).toHaveProperty('severity', 2)
-  expect(result[0]).toHaveProperty('message', 'Format Error: expected \"\\n\" ')
-})
+  it('lint invalid json', async () => {
+    const filename = './fixtures/demo.invalid.json'
+    const result = execute(filename, {
+      fix: true,
+    })
 
-it('lint invalid json', async () => {
-  const filename = './fixtures/demo.invalid.json'
-  const result = execute(filename, {
-    fix: true,
-  })
-
-  expect(result).toEqual({
-    filePath: path.join(__dirname, filename),
-    errorCount: 1,
-    fixableErrorCount: 0, 'fixableWarningCount': 0,
-    messages: [{
-      column: '3',
-      'fatal': true,
-      'line': '3',
-      'message': 'invalid character \'\\"\'',
-      ruleId: null, 'severity': 2,
-    }],
-    source: `{
+    expect(result).toEqual({
+      filePath: path.join(__dirname, filename),
+      errorCount: 1,
+      fixableErrorCount: 0,
+      fixableWarningCount: 0,
+      messages: [
+        {
+          column: '3',
+          fatal: true,
+          line: '3',
+          message: 'invalid character \'\\"\'',
+          ruleId: null,
+          severity: 2,
+        },
+      ],
+      source: `{
   "foo":1
   "bar": 2
 }
 
 `,
-    warningCount: 0,
+      warningCount: 0,
+    })
   })
 
-})
-
-it('lint bad json complex', async () => {
-  const filename = './fixtures/complex.json'
-  const result = execute(filename, {
-    fix: true,
-  })
-
-  expect(result.output).toBe(await getFormatted(filename))
-})
-
-it('lint bad json complex 1', async () => {
-  const filename = './fixtures/complex.1.json'
-  const result = execute(filename, {
-    fix: true,
-  })
-
-  expect(result.output).toBe(await getFormatted(filename))
-})
-
-it('lint .eslintrc', async () => {
-  const filename = './fixtures/.eslintrc'
-  const result = execute(filename, {
-    fix: true,
-  })
-
-  expect(result.output).toBe(await getFormatted(filename))
-
-})
-
-it('lint good json', () => {
-  const filename = './fixtures/demo.fixed.json'
-  const result = execute(filename, {
-    fix: true,
-  })
-
-  expect(result).toEqual({
-    filePath: path.join(__dirname, filename),
-    'errorCount': 0,
-    'fixableErrorCount': 0,
-    'fixableWarningCount': 0,
-    'messages': [],
-    'warningCount': 0,
-  })
-})
-
-it('lint + sort package.json', async () => {
-  const filename = './fixtures/__package.json'
-  const result = execute(filename, {
-    fix: true,
-  })
-
-  // console.log(result.output)
-
-  expect(result.output).toBe(`${formatJSON(sortPkgJSON(parseJSON(await getFormatted(filename))))}\n`)
-
-})
-
-it('lint + sort big package.json', async () => {
-  const filename = './fixtures/big/__package.json'
-  const result = execute(filename, {
-    fix: true,
-  })
-
-  // console.log(result.output)
-
-  expect(result.output).toBe(`${formatJSON(sortPkgJSON(parseJSON(await getFormatted(filename))))}\n`)
-
-})
-
-describe('configuration', () => {
-
-  it('not sort package.json when config', async () => {
-    const filename = './fixtures/__package.json'
+  it('lint bad json complex', async () => {
+    const filename = './fixtures/complex.json'
     const result = execute(filename, {
       fix: true,
-      'settings': {
-        'json/sort-package-json': false,
-      },
     })
 
     expect(result.output).toBe(await getFormatted(filename))
-
   })
 
-  it('leaves json-with-comments-filenames alone', async () => {
-    const filename = './fixtures/demo.json'
+  it('lint bad json complex 1', async () => {
+    const filename = './fixtures/complex.1.json'
     const result = execute(filename, {
       fix: true,
-      settings: { 'json/json-with-comments-filenames': ['demo.json'] },
     })
 
-    expect(result.output).toBe(undefined)
-  })
-})
-
-it('lint bad js', () => {
-  const res = execute('./fixtures/jsdemo.js', {
-    rules: {
-      'no-multiple-empty-lines': 'error',
-      'quotes': ['error', 'single'],
-
-    },
-    fix: true,
+    expect(result.output).toBe(await getFormatted(filename))
   })
 
-  expect(res.output).toEqual(`'foo'
+  it('lint .eslintrc', async () => {
+    const filename = './fixtures/.eslintrc'
+    const result = execute(filename, {
+      fix: true,
+    })
+
+    expect(result.output).toBe(await getFormatted(filename))
+  })
+
+  it('lint good json', () => {
+    const filename = './fixtures/demo.fixed.json'
+    const result = execute(filename, {
+      fix: true,
+    })
+
+    expect(result).toEqual({
+      filePath: path.join(__dirname, filename),
+      errorCount: 0,
+      fixableErrorCount: 0,
+      fixableWarningCount: 0,
+      messages: [],
+      warningCount: 0,
+    })
+  })
+
+  it('lint + sort package.json', async () => {
+    const filename = './fixtures/__package.json'
+    const result = execute(filename, {
+      fix: true,
+    })
+
+    expect(result.output).toBe(await getFormattedAndSorted(filename))
+  })
+
+  it('lint + sort big package.json', async () => {
+    const filename = './fixtures/big/__package.json'
+    const result = execute(filename, {
+      fix: true,
+    })
+
+    // console.log(result.output)
+
+    expect(result.output).toBe(await getFormattedAndSorted(filename))
+  })
+
+  it('lint + sort package.json in correct order', async () => {
+    const filename = './fixtures/__package.json'
+    const result = execute(filename, {
+      fix: true,
+    })
+
+    // starts out of order
+    expectOrder(_.keys(JSON.parse(await getFormatted(filename))), [
+      'license',
+      'dependencies',
+    ])
+
+    expect(result.output).toBe(await getFormattedAndSorted(filename))
+    const resultOrder = _.keys(JSON.parse(result.output))
+
+    // ends up in order
+    expectOrder(resultOrder, ['dependencies', 'license'])
+  })
+
+  describe('configuration', () => {
+    it('not sort package.json when config', async () => {
+      const filename = './fixtures/__package.json'
+      const result = execute(filename, {
+        fix: true,
+        settings: {
+          'json/sort-package-json': false,
+        },
+      })
+
+      expect(result.output).toBe(await getFormatted(filename))
+    })
+
+    it('ignores json-with-comments-files (for now)', async () => {
+      const filename = './fixtures/demo.json'
+      const result = execute(filename, {
+        fix: true,
+        settings: { 'json/json-with-comments-files': ['demo.json'] },
+      })
+
+      expect(result.warningCount).toBe(1)
+      expect(result.messages[0].message).toContain('json-with-comments-files')
+    })
+
+    it('ignores ignore-files', () => {
+      const filename = './fixtures/ignore-this-file.json'
+      const result = execute(filename, {
+        fix: true,
+        settings: { 'json/ignore-files': ['ignore-this-file.json'] },
+      })
+
+      expect(result.warningCount).toBe(1)
+      expect(result.messages[0].message).toContain('ignore-files')
+    })
+
+    it('ignores package-lock.json by default', () => {
+      const filename = './fixtures/package-lock.json'
+      const result = execute(filename)
+
+      expect(result.warningCount).toBe(1)
+      expect(result.messages[0].message).toContain('ignore-files')
+    })
+  })
+
+  it('lint bad js', () => {
+    const res = execute('./fixtures/jsdemo.js', {
+      rules: {
+        'no-multiple-empty-lines': 'error',
+        quotes: ['error', 'single'],
+      },
+      fix: true,
+    })
+
+    expect(res.output).toEqual(`'foo'
 
 
 // const one = 'foo'
 `)
-})
 
-it('lint bad js no fix', () => {
-  const res = execute('./fixtures/jsdemo.js', {
-    rules: {
-      'no-multiple-empty-lines': 'error',
-      'quotes': ['error', 'single'],
-
-    },
+    expect(res.messages).toHaveLength(0)
   })
 
-  expect(res[0]).toHaveProperty('severity', 2)
-})
-
-it('lint invalid js', () => {
-  const filename = './fixtures/jsdemo.invalid.js'
-  const res = execute(filename, {
-    rules: {
-      'no-multiple-empty-lines': 'error',
-      'quotes': ['error', 'single'],
-
-    },
-    fix: true,
-  })
-
-  expect(res).toEqual({
-    filePath: path.join(__dirname, filename),
-    'errorCount': 1,
-    'fixableErrorCount': 0,
-    'fixableWarningCount': 0,
-    'messages': [
-      {
-        'column': 1,
-        'fatal': true,
-        'line': 1,
-        'message': 'Parsing error: Unexpected token >>',
-        'ruleId': null,
-        'severity': 2,
+  it('lint bad js no fix', () => {
+    const res = execute('./fixtures/jsdemo.js', {
+      rules: {
+        'no-multiple-empty-lines': 'error',
+        quotes: ['error', 'single'],
       },
-    ],
-    'source': '>>\n',
-    'warningCount': 0,
+    })
+
+    expect(res.messages[0]).toHaveProperty('severity', 2)
+  })
+
+  it('lint invalid js', () => {
+    const filename = './fixtures/jsdemo.invalid.js'
+    const res = execute(filename, {
+      rules: {
+        'no-multiple-empty-lines': 'error',
+        quotes: ['error', 'single'],
+      },
+      fix: true,
+    })
+
+    expect(res).toEqual({
+      filePath: path.join(__dirname, filename),
+      errorCount: 1,
+      fixableErrorCount: 0,
+      fixableWarningCount: 0,
+      messages: [
+        {
+          column: 1,
+          fatal: true,
+          line: 1,
+          message: 'Parsing error: Unexpected token >>',
+          ruleId: null,
+          severity: 2,
+        },
+      ],
+      source: '>>\n',
+      warningCount: 0,
+    })
+  })
+
+  it('lint good js', () => {
+    const filename = './fixtures/jsdemo.fixed.js'
+    const res = execute(filename, {
+      rules: {
+        'no-multiple-empty-lines': 'error',
+        quotes: ['error', 'single'],
+      },
+      fix: true,
+    })
+
+    expect(res).toEqual({
+      filePath: path.join(__dirname, filename),
+      errorCount: 0,
+      fixableErrorCount: 0,
+      fixableWarningCount: 0,
+      messages: [],
+      warningCount: 0,
+    })
+  })
+
+  describe('errors', () => {
+    it('handles errors with good error message', () => {
+      jest.mock('eslint/lib/linter', () => false)
+      jest.resetModules()
+
+      let err
+
+      try {
+        require('..')
+      } catch (e) {
+        err = e
+      }
+
+      expect(err).toBeTruthy()
+      expect(err.message).toContain('ESLint version:')
+    })
+
+    describe('bad settings', () => {
+      it('json-with-comments-filenames', async () => {
+        const filename = './fixtures/demo.json'
+        const result = execute(filename, {
+          fix: true,
+          settings: { 'json/json-with-comments-filenames': ['demo.json'] },
+        })
+
+        expect(result.warningCount).toBe(1)
+        expect(result.messages[0].message).toMatchSnapshot()
+      })
+    })
+  })
+
+  const expectOrder = (arr, order) => {
+    expect(_.intersection(arr, order)).toEqual(order)
   }
-
-  )
-})
-
-it('lint good js', () => {
-  const filename = './fixtures/jsdemo.fixed.js'
-  const res = execute(filename, {
-    rules: {
-      'no-multiple-empty-lines': 'error',
-      'quotes': ['error', 'single'],
-
-    },
-    fix: true,
-  })
-
-  expect(res).toEqual({
-    filePath: path.join(__dirname, filename),
-    'errorCount': 0,
-    'fixableErrorCount': 0,
-    'fixableWarningCount': 0,
-    'messages': [],
-    'warningCount': 0,
-  })
 
 })
